d4899af7f87d5f2280866edef4c6b235cf166135
isaiah dahl <isaiahdahl@me.com>
2017-10-31 17:30:09 +0000

    clean up, nothing major

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ApplicationRecord
+  CATEGORIES = ["cat", "dog", "fish", "bird", "rabbit", "cow"]
+
+  validates :name, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
